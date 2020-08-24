5c02132005883e1c1ddd6d51f8c4c3587078aa23
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2020-08-24 12:34:05 -0300

    Remoção de registro

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ class Bd {
 				continue
 			}
 
+			despesa.id = i
 			despesas.push(despesa)
 		}
 
@@ -118,6 +119,11 @@ class Bd {
 		}
 
 		return despesasFiltradas
+	}
+
+	remover(id){
+
+		localStorage.removeItem(id)
 	}
 }
 
@@ -217,6 +223,29 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
 
 		linha.insertCell(2).innerHTML = d.descricao
 		linha.insertCell(3).innerHTML = d.valor
+
+		//botão para excluir registro
+		let btn = document.createElement("button")
+		btn.className = 'btn btn-danger'
+		btn.innerHTML = '<i class="fas fa-times"></i>'
+		btn.id = `id_despesa_${d.id}`
+
+		btn.onclick = function() {
+			//remove o registro do local storage
+			//alert('Teste do botão')
+			//alert(this.id)
+
+			let id = this.id.replace('id_despesa_', '')
+
+			bd.remover(id)
+
+			//recarregar a página
+			window.location.reload()
+
+		}
+		linha.insertCell(4).append(btn)
+
+		console.log(d)
 	})
 }
 
